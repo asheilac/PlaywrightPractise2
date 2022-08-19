dc022d8e4d030c0a7370e16d35aae02f354857fa
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-08-19 16:36:22 +0100

    added test for demoblaze login

--- tests/demoblaze.spec.ts
+++ tests/demoblaze.spec.ts
@@ -1,10 +1,18 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { DemoBlazePage } from './page-object-model/demoblaze-page';
 
-test('DemoBlaze homepage has cart',async ({page}) => {
-   
+test('DemoBlaze homepage has cart', async ({page}) => {   
     const demoBlazePage = new DemoBlazePage(page);
     await demoBlazePage.goto();
     await demoBlazePage.clickCart();
     await expect(page).toHaveURL(/.*cart.html/); 
-  });
+});
+
+test('DemoBlaze logs in', async({page}) => {
+    const demoBlazePage = new DemoBlazePage(page);
+    await demoBlazePage.goto();
+    await demoBlazePage.clickLogin();
+    await demoBlazePage.fillLoginFormWithValidDetails();
+    await demoBlazePage.clickSubmitLoginDetails();
+    await expect(page.locator('a',{hasText:'Welcome'})).toHaveText('Welcome sheila123');
+})
--- tests/page-object-model/demoblaze-page.ts
+++ tests/page-object-model/demoblaze-page.ts
@@ -2,11 +2,19 @@ import {Locator, Page} from '@playwright/test';
 
 export class DemoBlazePage{
     readonly page: Page;
-    readonly cartLink: Locator;
+    readonly cartLocator: Locator;
+    readonly loginLocator: Locator;
+    readonly usernameFieldLocator: Locator;
+    readonly passwordFieldLocator: Locator;
+    readonly submitLoginLocator: Locator;
     
     constructor(page:Page){
         this.page = page;
-        this.cartLink = page.locator('a',{hasText:'Cart'});
+        this.cartLocator = page.locator('a',{hasText:'Cart'});
+        this.loginLocator = page.locator('a',{hasText:'Log in'});
+        this.usernameFieldLocator = page.locator('input[id="loginusername"]');
+        this.passwordFieldLocator = page.locator('input[id="loginpassword"]');
+        this.submitLoginLocator = page.locator('button', {hasText: 'Log in'});        
     }
 
     async goto(){
@@ -14,6 +22,22 @@ export class DemoBlazePage{
     }
 
     async clickCart(){
-        await this.cartLink.first().click();
+        await this.cartLocator.first().click();
     }
+
+    async clickLogin(){
+        await this.loginLocator.first().click();
+    }
+
+    async fillLoginFormWithValidDetails(){
+        await this.usernameFieldLocator.fill('sheila123');
+        await this.passwordFieldLocator.fill('sheila123');
+    }
+
+    async clickSubmitLoginDetails(){
+        await this.submitLoginLocator.first().click();
+    }
+
+    
+
 }
