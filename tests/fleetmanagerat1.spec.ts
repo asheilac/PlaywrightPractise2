1d131b2ead3f86a6a78585106eb564486a174a40
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-08-26 15:19:07 +0100

    created tests for emulates a user and prints

--- tests/fleetmanagerat1.spec.ts
+++ tests/fleetmanagerat1.spec.ts
@@ -12,6 +12,31 @@ test('FleetManagerAt1 logs in', async({page}) => {
     await fleetManagerAt1Page.goto();
     await fleetManagerAt1Page.fillLoginFormWithValidDetails();
     await fleetManagerAt1Page.clickSubmitLoginDetails();
+    await expect(page).toHaveTitle(/Home/);
     await expect(page.locator('h1',{hasText:'Welcome'})).toHaveText('Welcome Sheila Coles');
+})
+
+test('FleetManagerAt1 emulates a user',async ({page}) => {
+    const fleetManagerAt1Page = new FleetManagerAt1(page);
+    await fleetManagerAt1Page.goto();
+    await fleetManagerAt1Page.fillLoginFormWithValidDetails();
+    await fleetManagerAt1Page.clickSubmitLoginDetails();
+    await page.goto('https://adsat1.ukho.gov.uk/UserDetails/Edit?userId=49987');
+    await fleetManagerAt1Page.clickEmulateThisUser();
+    await expect(page.locator('h1',{hasText:'Welcome'})).toHaveText('Welcome Confidence TestSio');
+})
+
+test('FleetManagerAt1 prints',async ({page}) => {
+    const fleetManagerAt1Page = new FleetManagerAt1(page);
+    await fleetManagerAt1Page.goto();
+    await fleetManagerAt1Page.fillLoginFormWithValidDetails();
+    await fleetManagerAt1Page.clickSubmitLoginDetails();
+    await page.goto('https://adsat1.ukho.gov.uk/UserDetails/Edit?userId=49987');
+    await fleetManagerAt1Page.clickEmulateThisUser();
+    await page.goto('https://adsat1.ukho.gov.uk/POD/PodPrintCharts');
+    await fleetManagerAt1Page.clickAddMazuChart();
+    await fleetManagerAt1Page.clickViewBasket();
+    await expect(page).toHaveTitle(/Print on Demand Basket/);
+    await expect(page.locator('td[aria-describedby="print-chart-table_Title"]')).toHaveText('Mazus Map Of The Ocean');
 })
 
--- tests/page-object-model/fleetmanagerat1-page.ts
+++ tests/page-object-model/fleetmanagerat1-page.ts
@@ -5,12 +5,19 @@ export class FleetManagerAt1{
     readonly submitLocator: Locator;
     readonly passwordFieldLocator: Locator;
     readonly usernameFieldLocator: Locator;
+    readonly emulateUserLocator: Locator;
+    readonly addMazuChartLocator: Locator;
+    readonly viewBasketLocator: Locator;
 
     constructor(page:Page){
         this.page = page;
         this.usernameFieldLocator = page.locator('input[name="username"]');
         this.passwordFieldLocator = page.locator('input[name="password"]');
         this.submitLocator = page.locator('text=Submit');
+
+        this.emulateUserLocator = page.locator('button', {hasText: 'Emulate This User'});        
+        this.addMazuChartLocator = page.locator("#btnIncQty150083");
+        this.viewBasketLocator = page.locator("#btnViewBasket");
     }
 
     async goto(){
@@ -24,5 +31,17 @@ export class FleetManagerAt1{
 
     async clickSubmitLoginDetails(){
         await this.submitLocator.first().click();
+    }
+
+    async clickEmulateThisUser(){
+        await this.emulateUserLocator.first().click();
+    }
+
+    async clickAddMazuChart(){
+        await this.addMazuChartLocator.first().click();
+    }
+
+    async clickViewBasket(){
+        await this.viewBasketLocator.first().click();
     }
 }
